9e375240f64a7dd4f4a42cac8ca47b5a3ff8110e
mertcancet <mertcancet95@gmail.com>
2025-11-12 16:26:36 +0300

    asd

--- app/(home)/kvkk-metni/page.tsx
+++ app/(home)/kvkk-metni/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const KVKKMetni = () => {
   return (
     <div className="max-w-4xl mx-auto px-4 py-8 text-gray-800">
-      <h1 className="text-3xl font-bold mb-6">KVKK Aydınlatma Metni</h1>
+      <h1 className="text-3xl font-bold mb-6">KVKK Aydınlatma Metni </h1>
       <h2 className="text-2xl font-semibold">1. Giriş</h2>
       <p className="mb-4">
         Planix (“Planix” veya “Şirket”) olarak, kullanıcılarımızın, iş
